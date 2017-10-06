21e3a99e20d977a53250df13eedede76330ca7a9
davicardoso <davi.almeida.c@gmail.com>
2017-10-06 12:24:17 -0300

    PessoaDAO e ReservaDAO criados

--- Hotel Web/src/java/br/com/ufjf/dcc078/DAO/PessoaDAO.java
+++ Hotel Web/src/java/br/com/ufjf/dcc078/DAO/PessoaDAO.java
@@ -5,10 +5,107 @@
  */
 package br.com.ufjf.dcc078.DAO;
 
+import br.com.ufjf.dcc078.Modelo.Pessoa;
+import br.com.ufjf.dcc078.persistencia.DatabaseLocator;
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
 /**
  *
  * @author Desenvolvedor
  */
 public class PessoaDAO {
     
+    private static PessoaDAO instance;
+    
+    public static PessoaDAO getInstance() {
+        if (instance == null) {
+            instance = new PessoaDAO();
+        }
+        return instance;
+    }
+    
+    private PessoaDAO() {
+        
+    }
+    
+    public void gravar(Pessoa pessoa) throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        Statement st = null;
+        try {
+            conn = DatabaseLocator.getInstance().getConnection();
+            st = conn.createStatement();
+            st.execute("insert into pessoa (nome,cpf, endereco, tipo_pessoa, email)"
+                    + " values ('" + pessoa.getNome() + "', '" + pessoa.getCpf() + "', '"
+                    + pessoa.getEndereco() + "', '" + pessoa.getTipo_pessoa() + "','"
+                    + pessoa.getEmail() + "')");
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            closeResources(conn, st);
+        }
+    }
+    
+    public void excluir(int id) throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        PreparedStatement st = null;
+        try {
+            conn = DatabaseLocator.getInstance().getConnection();
+            st = conn.prepareStatement("DELETE FROM pessoa WHERE id = ?");
+            st.setInt(1, id);
+            st.execute();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            closeResources(conn, st);
+        }
+    }
+    
+    public List<Pessoa> lerTodos() throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        PreparedStatement st = null;
+        ResultSet rs;
+        List<Pessoa> pessoas = new VirtualFlow.ArrayLinkedList<>();
+        try {
+            conn = DatabaseLocator.getInstance().getConnection();
+            st = conn.prepareStatement("SELECT * FROM pessoa");
+            rs = st.executeQuery();
+            
+            while (rs.next()) {
+                Pessoa pessoa = new Pessoa();
+                pessoa.setId(rs.getInt("id"));
+                pessoa.setNome(rs.getString("nome"));
+                pessoa.setCpf(rs.getString("cpf"));
+                pessoa.setEndereco(rs.getString("endereco"));
+                pessoa.setTipo_pessoa(rs.getString("tipo_pessoa").charAt(0));
+                pessoa.setEmail(rs.getString("email"));
+                pessoas.add(pessoa);
+            }
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            closeResources(conn, st);
+            
+        }
+        return pessoas;
+    }
+    
+    private void closeResources(Connection conn, Statement st) {
+        try {
+            if (st != null) {
+                st.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+            
+        } catch (SQLException e) {
+            
+        }
+    }
 }
--- Hotel Web/src/java/br/com/ufjf/dcc078/DAO/ReservaDAO.java
+++ Hotel Web/src/java/br/com/ufjf/dcc078/DAO/ReservaDAO.java
@@ -5,10 +5,107 @@
  */
 package br.com.ufjf.dcc078.DAO;
 
+import br.com.ufjf.dcc078.Modelo.Reserva;
+import br.com.ufjf.dcc078.persistencia.DatabaseLocator;
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
 /**
  *
  * @author Desenvolvedor
  */
 public class ReservaDAO {
-    
+
+    private static ReservaDAO instance;
+
+    private ReservaDAO() {
+
+    }
+
+    public static ReservaDAO getInstance() {
+        if (instance == null) {
+            instance = new ReservaDAO();
+        }
+        return instance;
+    }
+
+    public void gravar(Reserva reserva) throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        Statement st = null;
+        try {
+            conn = DatabaseLocator.getInstance().getConnection();
+            st = conn.createStatement();
+            st.execute("insert into reserva (pessoa_id,quarto_id, data_checkin, data_checkout, data_reserva)"
+                    + " values ('" + reserva.getPessoa_id() + "', '" + reserva.getQuarto_id() + "', '"
+                    + reserva.getData_checkin() + "', '" + reserva.getData_checkout() + "','"
+                    + reserva.getData_reserva() + "')");
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            closeResources(conn, st);
+        }
+    }
+
+    public void excluir(int id) throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        PreparedStatement st = null;
+        try {
+            conn = DatabaseLocator.getInstance().getConnection();
+            st = conn.prepareStatement("DELETE FROM reserva WHERE id = ?");
+            st.setInt(1, id);
+            st.execute();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            closeResources(conn, st);
+        }
+    }
+
+    public List<Reserva> lerTodos() throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        PreparedStatement st = null;
+        ResultSet rs;
+        List<Reserva> reservas = new VirtualFlow.ArrayLinkedList<>();
+        try {
+            conn = DatabaseLocator.getInstance().getConnection();
+            st = conn.prepareStatement("SELECT * FROM reserva");
+            rs = st.executeQuery();
+
+            while (rs.next()) {
+                Reserva reserva = new Reserva();
+                reserva.setId(rs.getInt("id"));
+                reserva.setPessoa_id(rs.getInt("id_pessoa"));
+                reserva.setQuarto_id(rs.getInt("id_quarto"));
+                reserva.setData_checkin(rs.getString("data_checkin"));
+                reserva.setData_checkout(rs.getString("data_checkout"));
+                reserva.setData_reserva(rs.getString("data_reserva"));
+                reservas.add(reserva);
+            }
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            closeResources(conn, st);
+
+        }
+        return reservas;
+    }
+
+    private void closeResources(Connection conn, Statement st) {
+        try {
+            if (st != null) {
+                st.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+
+        } catch (SQLException e) {
+
+        }
+    }
 }
