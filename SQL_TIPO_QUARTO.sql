28462c9dbe56d00f043d6b2de9d1be3aa11d3c87
Ana Carolina Fidelis Gonçalves <ana@codejr.com.br>
2017-10-06 14:35:33 -0300

    Modificação no banco de dados e acrescimo do script de inserção dos tipos de quarto.

---
+++ SQL_TIPO_QUARTO.sql
@@ -0,0 +1,4 @@
+INSERT INTO `tipo_quarto` (`id`,`nome`,`valor`,`max_pessoas`) VALUES (1,'Casal',100.00,2);
+INSERT INTO `tipo_quarto` (`id`,`nome`,`valor`,`max_pessoas`) VALUES (2,'Solteiro',50.00,1);
+INSERT INTO `tipo_quarto` (`id`,`nome`,`valor`,`max_pessoas`) VALUES (3,'Duplo',150.00,3);
+INSERT INTO `tipo_quarto` (`id`,`nome`,`valor`,`max_pessoas`) VALUES (4,'Família',200.00,4);
