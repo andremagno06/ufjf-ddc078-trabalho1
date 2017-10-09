9f51f00c4820113f3f8f8a060a33d023fadcce17
davicardoso <davi.almeida.c@gmail.com>
2017-10-09 16:55:43 -0300

    Template iniciado

--- Hotel Web/src/java/br/com/ufjf/dcc078/Actions/InserirQuartoAction.java
+++ Hotel Web/src/java/br/com/ufjf/dcc078/Actions/InserirQuartoAction.java
@@ -4,7 +4,10 @@ import br.com.ufjf.dcc078.Controller.Action;
 import br.com.ufjf.dcc078.DAO.QuartoDAO;
 import br.com.ufjf.dcc078.Modelo.Quarto;
 import br.com.ufjf.dcc078.Modelo.QuartoCasal;
+import br.com.ufjf.dcc078.Modelo.QuartoDuplo;
 import br.com.ufjf.dcc078.Modelo.QuartoEstadoDisponivel;
+import br.com.ufjf.dcc078.Modelo.QuartoFamilia;
+import br.com.ufjf.dcc078.Modelo.QuartoSolteiro;
 import java.io.IOException;
 import java.sql.SQLException;
 import javax.servlet.http.HttpServletRequest;
@@ -30,11 +33,16 @@ public class InserirQuartoAction implements Action {
             switch(Integer.parseInt(tipo)){
                 case 1:
                     quarto =new QuartoCasal(descricao, new QuartoEstadoDisponivel(), Integer.parseInt(tipo));
+                    break;
                 case 2:
-                    
+                    quarto =new QuartoSolteiro(descricao, new QuartoEstadoDisponivel(), Integer.parseInt(tipo));
+                    break;
                 case 3:
-                    
-                case 4:    
+                    quarto =new QuartoDuplo(descricao, new QuartoEstadoDisponivel(), Integer.parseInt(tipo));
+                    break;
+                case 4:   
+                    quarto =new QuartoFamilia(descricao, new QuartoEstadoDisponivel(), Integer.parseInt(tipo));
+                    break;
             }
             try {
                 QuartoDAO.getInstance().gravar(quarto);
