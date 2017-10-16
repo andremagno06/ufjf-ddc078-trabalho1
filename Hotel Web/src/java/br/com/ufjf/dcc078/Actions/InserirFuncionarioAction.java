a21e17f5da3b9470525ffa47a79ea83e11009f32
Ana Carolina Fidelis Gonçalves <ana@codejr.com.br>
2017-10-15 23:11:46 -0200

    Adição de importação do observer em inseri funcionário.

--- Hotel Web/src/java/br/com/ufjf/dcc078/Actions/InserirFuncionarioAction.java
+++ Hotel Web/src/java/br/com/ufjf/dcc078/Actions/InserirFuncionarioAction.java
@@ -7,6 +7,7 @@ import br.com.ufjf.dcc078.Modelo.PessoaFuncionario;
 import br.com.ufjf.dcc078.Modelo.Quarto;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Observer;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
